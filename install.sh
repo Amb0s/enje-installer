45179489b149ad9277bbb15ce3cb599e19622bd3
Amb0s <ambos@disroot.org>
2022-12-31 06:34:43 +0100

    Changed package list

--- install.sh
+++ install.sh
@@ -3,20 +3,20 @@
 # Removes useless distribution packages.
 sudo pacman -Rns --noconfirm baobab cheese eog epiphany gnome-backgrounds \
     gnome-calculator gnome-contacts gnome-logs gnome-maps gnome-music gnome-photos \
-    gnome-remote-desktop nautilus gnome-text-editor gnome-software \
+    gnome-remote-desktop nautilus gnome-text-editor gnome-software gnome-console \
     gnome-system-monitor gnome-weather simple-scan malcontent orca totem vim htop
 
 # Installs distribution packages.
-sudo pacman -S --noconfirm --needed base-devel ufw neofetch firefox \
+sudo pacman -S --noconfirm --needed base-devel gnome-terminal ufw neofetch \
     torbrowser-launcher cmus milkytracker mat2 discord mpv hugo transmission-gtk \
     thunar thunar-archive-plugin neovim pacman-contrib aegisub obs-studio blender \
     gimp inkscape btop weechat profanity qtox keepassxc monero-gui veracrypt gamemode \
     reflector newsboat ueberzug aerc yt-dlp intellij-idea-community-edition mednafen \
-    bleachbit man-db man-pages shotcut python-pywal tmux gnome-themes-extra bc maim \
+    bleachbit fwupd man-db man-pages shotcut python-pywal tmux gnome-themes-extra bc \
     steam tumbler lynx jdk8-openjdk ffmpegthumbnailer adobe-source-code-pro-fonts \
     awesome-terminal-fonts ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans \
     ttf-iosevka-nerd ttf-ubuntu-font-family ttf-liberation ttf-cascadia-code \
-    ttf-fira-code ttf-hack-nerd terminus-font sxiv amd-ucode
+    ttf-fira-code ttf-hack-nerd terminus-font task sxiv amd-ucode nvidia-dkms #gamescope
 
 # Installs AUR helper.
 git clone https://aur.archlinux.org/yay.git
@@ -25,7 +25,7 @@ makepkg -si
 
 # Installs AUR packages
 yay -S --noconfirm --sudoloop --answerclean All --answerdiff None --removemake \
-    --mflags "--skipchecksums --skippgpcheck" unityhub gnome-browser-connector-git \
+    --mflags "--skipchecksums --skippgpcheck" librewolf-bin unityhub gnome-browser-connector-git \
     lf-git sc-im packwiz-git xmrig-cuda tilde xmrig-donateless stagit dosbox-x tmsu \
     mpv-sponsorblock-minimal-git protonup-git gradience papirus-folders xcursor-simp1e \
     gnome-shell-extension-disable-unredirect-git gnome-shell-extension-simply-workspaces-git \
@@ -34,9 +34,7 @@ yay -S --noconfirm --sudoloop --answerclean All --answerdiff None --removemake \
     gnome-shell-extension-openweather-git gallery-dl bombadillo adw-gtk3 nerd-fonts-go-mono \
     jdk17-temurin nerd-fonts-inconsolata nerd-fonts-jetbrains-mono nerd-fonts-mononoki \
     nerd-fonts-roboto-mono ttf-ms-fonts ttf-vista-fonts ttf-tahoma ttf-segoe-ui-variable \
-    inxi-git gnome-shell-extension-rounded-window-corners-git
-
-# additional extensions: panel date format, krypto, disable workspace switcher
+    inxi-git vopono pyload-ng gnome-shell-extension-rounded-window-corners-git tenacity-git
 
 # Removes orphans.
 sudo pacman -Rns --noconfirm $(pacman -Qtdq)
